4825e81efe04c69a4401fdec6b079cb7407880f3
time9v <lol.time9@gmail.com>
2024-02-26 13:36:46 +0300

    v 0.2

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,6 +6,9 @@ def main():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
+    parser.add_argument('-f', '--format', metavar='FORMAT',
+                        type=str, default='stylish',
+                        help='set format for output')
     args = parser.parse_args()
     print(args.indir)
 
